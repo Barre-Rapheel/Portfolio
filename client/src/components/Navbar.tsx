c5abf1d5034a50200b20bcbeb63b86efecfbb9bf
Raphaël <barre.raphael97@gmail.com>
2025-02-14 17:26:03 +0100

    Correction

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projets</Link>
+        </li>
+        <li>
+          <Link to="/about">À propos</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
