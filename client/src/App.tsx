669ecee19d8240c224a19487f6b7bfcf3ee12bb8
Raphaël <barre.raphael97@gmail.com>
2025-02-19 17:53:41 +0100

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
